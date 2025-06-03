b3424c64689bbb013cb8b7e4623f59e87e139406
atikxm <atikurrahmanxm@gmail.com>
2025-06-04 00:07:08 +0530

    learn if, else , elif

---
+++ Conditionals_Operators.py
@@ -0,0 +1,30 @@
+'''
+input =int (input ("Enter Your age:"))
+
+if input >= 18 :
+    print("Tumi vote dite parba")
+
+else:
+    print("Tumi vote dite parbe na r o boyosh lagbe")
+'''
+#COnditionals and Operators
+'''
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Tumi vote dite parba.")
+else:
+    print("Tumi vote dite parba na.")
+
+'''
+
+mark = 65
+
+if mark >= 80:
+    print("A+")
+elif mark >= 70:
+    print("A")
+elif mark >= 60:
+    print("B")
+else:
+    print("Fail")
+
